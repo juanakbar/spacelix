f212b3beb66cd6605d77050f476b230b38281721
juanakbar <juanindrian02@gmail.com>
2023-01-12 07:58:29 +0700

    updateLagi

--- src/Pages/Tutorial/Tutorial.jsx
+++ src/Pages/Tutorial/Tutorial.jsx
@@ -26,7 +26,7 @@ export default function Tutorial(props) {
                 <div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6'>
                     <div className='mx-auto max-w-screen-sm text-center lg:mb-16 mb-8'>
                         <h2 className='mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>Our
-                            Blog</h2>
+                            Tutroial</h2>
                         <p className='font-light text-gray-500 sm:text-xl dark:text-gray-400'>Let's learn without limit</p>
                     </div>
                     <div className='grid gap-8 lg:grid-cols-2 container'>
