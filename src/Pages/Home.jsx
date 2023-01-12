eed5ed7a945d47c3e455675ac255e10a8213a9b5
juanakbar <juanindrian02@gmail.com>
2023-01-12 13:29:51 +0700

    Fix Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -29,7 +29,7 @@ export default function Home(props) {
                                         </span>
                                     <span
                                         className='pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200'>
-                                            <span>See what's new</span> &rarr;
+                                            <span className={'sm:inline-block hidden'}>See what's new</span> &rarr;
                                     </span>
                                 </a>
                             </div>
@@ -39,7 +39,7 @@ export default function Home(props) {
 
                 <div className='flex justify-center mt-10'>
                     <img className='object-cover w-full h-96 rounded-xl lg:w-4/5'
-                         src='https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80' />
+                         src='/img/Home.jpeg' />
                 </div>
             </div>
             <Team />
