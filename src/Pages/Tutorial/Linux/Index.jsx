f68daedc0e82b65eb8d3bbd823282924f84f4248
juanakbar <juanindrian02@gmail.com>
2023-01-12 08:17:40 +0700

    updateLagi

--- src/Pages/Tutorial/Linux/Index.jsx
+++ src/Pages/Tutorial/Linux/Index.jsx
@@ -27,7 +27,7 @@ export default function Linux(props) {
                             <div
                                 className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                                 <a href={item.href}>
-                                    <img className="rounded-t-lg" src={`/src/assets/Article/${item.img}`} alt={item.name} />
+                                    <img className="rounded-t-lg" src={`./src/assets/Article/${item.img}`} alt={item.name} />
                                 </a>
                                 <div className="p-5">
                                     <a href={item.href}>
